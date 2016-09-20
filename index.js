b8c3dfd412dc9ea2c91faabc1c375af4c7e50696
napon <napon@napontaratan.com>
2016-09-20 21:59:48 +0000

    Updated path

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var gith = require('gith').create(9001);
+var path = require('path');
 var execFile = require('child_process').execFile;
+var filePath = path.resolve('/home/napon/github-auto-deploy/', './pull-and-deploy.sh');
 
 console.log('Listening on 9001..');
 
 gith({
-    repo: 'https://github.com/napon/github-auto-deploy.git'
-}).on('all', function(payload) {
-    if (payload.branch == 'master') {
-        execFile('pull-and-deploy.sh', function(error, stdout, stderr) {
-            console.log(stdout);
-        });
-    }
+    repo: 'napon/napontaratan.com'
+}).on('file:all', function(payload) {
+    console.log('Received a payload. ' + new Date());
+    execFile(filePath, ['/usr/share/nginx/', 'napontaratan.com'], function(error, stdout, stderr) {
+	console.log('Started script.');
+        console.log(stdout, error, stderr);
+    });
+    console.log('Done.');
 });
